cadf12590ce984e73c7fcffeffde8acac8e96d60
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2023-08-30 09:38:30 -0300

    Modelagem tática
    Foi desenvolvido com base no modulo "Mão na massa - Modelagem tática" foram abordados os seguintes tópicos:
    - Criação da estrutura do projeto.
    - Criação do contexto Catalogo.
    - Criação do núcleo compartilhado identificado como Core.
    - Criação da camada Aplicação do Catalogo
    - Criação da camada Data do Catalogo
    - Criação da camada Domínio do Catalogo
    - Criação da camada Apresentação
    - Execução das migrations

---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,41 @@
+﻿namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; private set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            // If both are null, or both are same instance, return true.
+            if (ReferenceEquals(a, b)) return true;
+
+            // If one is null, but not both, return false.
+            if (ReferenceEquals(null, a)) return false;
+            if (ReferenceEquals(null, b)) return false;
+
+            // Return true if the fields match:
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b) => !(a == b);
+
+        public override int GetHashCode() => (GetType().GetHashCode() * 907) + Id.GetHashCode();
+
+        public override string ToString() => $"{GetType().Name} [Id={Id}]";
+    }
+}
---
+++ tests/NerdStore.Catalogo.Domain.Tests/DimensoesTests.cs
@@ -0,0 +1,33 @@
+﻿using NerdStore.Catalogo.Domain.Entities;
+using NerdStore.Catalogo.Domain.ValueObjects;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain.Tests
+{
+    public class DimensoesTests
+    {
+        [Fact]
+        public void Dimensoes_Validar_ValidacaoAlturaDeveRetornarExceptions()
+        {
+            var ex = Assert.Throws<DomainException>(() => new Dimensoes(0, 1, 1));
+
+            Assert.Equal("O campo Altura não pode ser menor ou igual a 0", ex.Message);
+        }
+
+        [Fact]
+        public void Dimensoes_Validar_ValidacaoLarguraDeveRetornarExceptions()
+        {
+            var ex = Assert.Throws<DomainException>(() => new Dimensoes(1, 0, 1));
+
+            Assert.Equal("O campo Largura não pode ser menor ou igual a 0", ex.Message);
+        }
+
+        [Fact]
+        public void Dimensoes_Validar_ValidacaoProfundidadeDeveRetornarExceptions()
+        {
+            var ex = Assert.Throws<DomainException>(() => new Dimensoes(1, 1, 0));
+
+            Assert.Equal("O campo Profundidade não pode ser menor ou igual a 0", ex.Message);
+        }
+    }
+}
